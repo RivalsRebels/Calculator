8996f03cfd1690563dfa0c0002f733b1efcd29ee
gabia <gabia@DESKTOP-78555EL>
2022-10-26 18:35:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MainCal
+{
+    public class Program
+    {
+
+        static void Main(String[] args)
+        {
+            Console.Write("Press the First Number: ");
+            double Number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Press the Sign: ");
+            string op = Console.ReadLine();
+            Console.Write("Press the Second Number: ");
+            double Number2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine( Number1 + Number2 );
+            }
+            else if(op == "-")
+            {
+                Console.WriteLine( Number1 - Number2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(Number1 * Number2);
+            }
+
+
+
+        }
+
+
+
+    }
+}
